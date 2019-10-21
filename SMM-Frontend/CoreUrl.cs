1e9f312fc48702d33ba0618879d530606eaf7330
yyc12345 <yyc12321@outlook.com>
2019-10-21 17:44:16 +0800

    change dotnet version and create utility lib

---
+++ SMM-Frontend/CoreUrl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SMM_Frontend {
+    internal static class CoreUrl {
+
+        internal static string CoreURL = "http://yyc.bkt.moe:6666/";
+
+        internal static string Salt => CoreURL + "salt.php";
+
+        internal static string Login => CoreURL + "login.php";
+
+        internal static string Logout => CoreURL + "logout.php";
+
+    }
+}
---
+++ SMM-Frontend/Method.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SMM_Frontend.Structure;
+
+namespace SMM_Frontend {
+    public static class CoreMethod {
+
+        
+
+        public static void ChangeDomain(string str) {
+            
+        }
+
+
+    }
+}
