443e133045d22cf08a6e18a4049d57be13ba99e8
yyc12345 <yyc12321@outlook.com>
2019-12-19 22:26:06 +0800

    finish map and mapPool interface

--- SMM-Backend/operationMapPool.php
+++ SMM-Backend/operationMapPool.php
@@ -5,23 +5,76 @@ require_once "utilities.php";
 require_once "databasehelper.php";
 
 try {
-    if (!\SMMUtilities\CheckNecessityParam($_POST, array("name"))) throw new Exception("Invalid parameter");
+    $db = new database();
+    $db->lockdb();
+
+    //confirm token and permission
+    if (!\SMMUtilities\CheckNecessityParam($_POST, array("token"))) throw new Exception("Invalid parameter");
+    if(!$db->checkToken($_POST["token"])) throw new Exception("Invalid token");
+    if(!(\SMMUtilities\CheckPriority($db->getPriority($_POST["token"]), \SMMDataStructure\EnumUserPriority::admin))) throw new Exception("No permission");
 
     if (\SMMUtilities\CheckHardcodeParam($_POST, array("method" => "query"))) {
-        //query
+        //query, check param
+        if (!\SMMUtilities\CheckNecessityParam($_POST, array("filterRules"))) throw new Exception("Invalid parameter");
+        //decode filter rules and needed return
+        $decodeFilter = \SMMUtilities\AdvancedJsonArrayDecoder($_POST["filterRules"]);
 
+        //contruct statement
+        $whereStatement = "";
+        $args = array();
+        \SMMDatabaseStatement\GenerateFilterStatement($decodeFilter, array(
+            "hash" => new \SMMDatabaseStatement\ParamFilterUserInput('=', PDO::PARAM_STR, "sm_hash"),
+            "tournament" => new \SMMDatabaseStatement\ParamFilterUserInput('=', PDO::PARAM_STR, "sm_tournament")
+        ),array(), $whereStatement, $args);
 
+        //bind param and execute
+        $stmt = $db->conn->prepare('SELECT * FROM mapPool' . ( $whereStatement == "" ? "" : " WHERE " . $whereStatement));
+        foreach($args as $key=>$value) $stmt->bindParam($key+1, $value->paramValue, $value->paramSQLType);
+        $stmt->execute();
+
+        echo json_encode(\SMMUtilities\GetUniversalReturn(true, "OK", $stmt->fetchAll(PDO::FETCH_ASSOC)));
 
     } else if (\SMMUtilities\CheckHardcodeParam($_POST, array("method" => "add"))) {
-        //add
+        //add, check param
+        if(!\SMMUtilities\CheckNecessityParam($_POST, array("newValues"))) throw new Exception("Invalid parameter");
+        $decodeNewValues = \SMMUtilities\AdvancedJsonArrayDecoder($_POST["newValues"]);
+        if(!\SMMUtilities\CheckNecessityParam($decodeNewValues, array("hash", "tournament"))) throw new Exception("Invalid parameter");
 
+        //construct statement
+        $insertKeyStatement = "";
+        $insertValueStatement = "";
+        $args = array();
+        \SMMDatabaseStatement\GenerateSeparatedStatement($decodeNewValues, 
+        array("hash" => new \SMMDatabaseStatement\ParamSeperatedUserInput(PDO::PARAM_STR, "sm_hash"),
+            "tournament" => new \SMMDatabaseStatement\ParamSeperatedUserInput(PDO::PARAM_STR, "sm_tournament")),
+        array(), $insertKeyStatement, $insertValueStatement, $args);
 
-        
+        //bind param and execute
+        $stmt = $db->conn->prepare('INSERT mapPool ' . $insertKeyStatement . ' VALUES ' . $insertValueStatement);
+        foreach($args as $key=>$value) $stmt->bindParam($key+1, $value->paramValue, $value->paramSQLType);
+        $stmt->execute();
+
+        echo json_encode(\SMMUtilities\GetUniversalReturn());
+
     } else if (\SMMUtilities\CheckHardcodeParam($_POST, array("method" => "delete"))) {
-        //rm
+        //rm, check param
+        if(!\SMMUtilities\CheckNecessityParam($_POST, array("target"))) throw new Exception("Invalid parameter");
+        $decodeTarget = \SMMUtilities\AdvancedJsonArrayDecoder($_POST["target"]);
+        if(count($decodeTarget) == 0) throw new Exception("Zero target is not allowed");
 
+        //construct statement
+        $whereStatement = "";
+        $args = array();
+        \SMMDatabaseStatement\GenerateFilterStatement(array(), array(),
+        array("sm_hash" => new \SMMDatabaseStatement\ParamFilterConstantInput("=", PDO::PARAM_STR, $decodeTarget)),
+        $whereStatement, $args);
+        //bind param and execute
+        $stmt = $db->conn->prepare('DELETE FROM mapPool WHERE ' . $whereStatement);
+        foreach($args as $key=>$value) $stmt->bindParam($key+1, $value->paramValue, $value->paramSQLType);
+        $stmt->execute();
 
-        
+        echo json_encode(\SMMUtilities\GetUniversalReturn());
+
     } else throw new Exception("Invalid parameter");
 } catch (Exception $e) {
     echo json_encode(\SMMUtilities\GetUniversalReturn(false, $e->getMessage()));
