435c04f41f57477607e37cb5e72513f43d14130b
yyc12345 <yyc12321@outlook.com>
2019-12-24 11:13:50 +0800

    finish mahoushoujo UI and fix backend bug

--- SMM-Backend/operationCompetition.php
+++ SMM-Backend/operationCompetition.php
@@ -125,7 +125,7 @@ try {
             $stmt2->execute();
         }
 
-        echo json_encode(\SMMUtilities\GetUniversalReturn());
+        echo json_encode(\SMMUtilities\GetUniversalReturn(true, "OK", $lastID));
 
     } else if (\SMMUtilities\CheckHardcodeParam($_POST, array("method" => "delete"))) {
         //rm, check param
